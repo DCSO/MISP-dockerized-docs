2c8a5191c9012173fbce45d3e7db4af442451fa7
8ear <github@8ear.de>
2018-10-24 18:20:29 +0200

    Separate build html and push

--- .travis/execute_CI_tasks.sh
+++ .travis/execute_CI_tasks.sh
@@ -52,10 +52,6 @@ else
 fi
 echo "#########################################################"  
 
-
-git --version
-git remote add -t gh-pages origin_MAIN git@github.com:DCSO/MISP-dockerized.git
-
 # install: 
 export PATH="$PATH:/$(whoami)/.local/bin"
 pip install -r ../requirements.txt
@@ -69,31 +65,5 @@ echo
 make -C ../ html
 echo "#########################################################"
 echo "#########################################################"  
-echo "#########################################################"  
-echo
-
-
-echo -n "try to upload..."
-# ghp-import Options:
-#   -n, --no-jekyll       Include a .nojekyll file in the branch.
-#   -c CNAME, --cname=CNAME
-#                         Write a CNAME file with the given CNAME.
-#   -m MESG, --message=MESG
-#                         The commit message to use on the target branch.
-#   -p, --push            Push the branch to origin/{branch} after committing.
-#   -f, --force           Force the push to the repository
-#   -r REMOTE, --remote=REMOTE
-#                         The name of the remote to push to. [origin]
-#   -b BRANCH, --branch=BRANCH
-#                         Name of the branch to write to. [gh-pages]
-#   -s, --shell           Use the shell when invoking Git. [False]
-#   -l, --follow-links    Follow symlinks when adding files. [False]
-#   -h, --help            show this help message and exit
-ghp-import -n -m "Update MISP-dockerized-docs" -f -p ../_build/html
-#ghp-import -n -m "Update MISP-dockerized-docs" -p -s -r origin_MAIN ../_build/html
-echo "finished."
-
-
-echo "#########################################################"
 echo "#########################################################"  
 echo
--- .travis/push.sh
+++ .travis/push.sh
@@ -1,33 +1,35 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
+
+
+
+git --version
+git remote add -t gh-pages origin_MAIN git@github.com:DCSO/MISP-dockerized.git
+git remote add -t gh-pages origin_GITHUB git@github.com:DCSO/MISP-dockerized-docs.git
+
+
 # Push HTML files to gh-pages automatically.
 
-# Fill this out with the correct org/repo
-ORG=dcso
-REPO=MISP-dockerized-docs
-# This probably should match an email for one of your users.
-EMAIL=no-reply@dcso.de
+echo -n "try to upload..."
+# ghp-import Options:
+#   -n, --no-jekyll       Include a .nojekyll file in the branch.
+#   -c CNAME, --cname=CNAME
+#                         Write a CNAME file with the given CNAME.
+#   -m MESG, --message=MESG
+#                         The commit message to use on the target branch.
+#   -p, --push            Push the branch to origin/{branch} after committing.
+#   -f, --force           Force the push to the repository
+#   -r REMOTE, --remote=REMOTE
+#                         The name of the remote to push to. [origin]
+#   -b BRANCH, --branch=BRANCH
+#                         Name of the branch to write to. [gh-pages]
+#   -s, --shell           Use the shell when invoking Git. [False]
+#   -l, --follow-links    Follow symlinks when adding files. [False]
+#   -h, --help            show this help message and exit
+ghp-import -n -m "Update MISP-dockerized-docs" -f -p -r origin_GITHUB ../_build/html
+#ghp-import -n -m "Update MISP-dockerized-docs" -p -s -r origin_MAIN ../_build/html
+echo "finished."
 
-set -e
 
-# Clone the gh-pages branch outside of the repo and cd into it.
-cd ..
-git clone -b gh-pages "https://$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
-cd gh-pages
-
-# Update git configuration so I can push.
-if [ "$1" != "dry" ]; then
-    # Update git config.
-    git config user.name "Automatic Builder"
-    git config user.email "$EMAIL"
-fi
-
-# Copy in the HTML.  You may want to change this with your documentation path.
-cp -R ../$REPO/_build/html/* ./
-
-# Add and commit changes.
-git add -A .
-git commit -m "[ci skip] Autodoc commit for $COMMIT."
-if [ "$1" != "dry" ]; then
-    # -q is very important, otherwise you leak your GH_TOKEN
-    git push -q origin gh-pages
-fi
+echo "#########################################################"
+echo "#########################################################"  
+echo
